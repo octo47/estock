e13542dfe390a3b3aaf02646c02beb91956e79f9
Andrey Stepachev <octo@yandex-team.ru>
2012-04-30 01:25:23 +0400

    support for min/max prices in random walker

--- apps/estockd/src/estockd_generator.erl
+++ apps/estockd/src/estockd_generator.erl
@@ -17,19 +17,22 @@
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3, rt_generator/1]).
+		 terminate/2, code_change/3, rt_generator/1]).
 
 -include("estockd.hrl").
 
 -define(SERVER, ?MODULE). 
+-define(STEPS, 6).
 
 -record(state, {}).
 -record(rt_state, { 
-	  name :: string(), 
-	  price :: float(), 
-	  volatility :: float(),
-	  frequency :: integer()
-	 }).
+		  name :: string(), 
+		  price :: float(), 
+		  volatility :: float(),
+		  frequency :: integer(),
+		  min_price :: float(),
+		  max_price :: float()
+		 }).
 
 %%%===================================================================
 %%% API
@@ -45,13 +48,17 @@ start_link() ->
 init([]) ->
     {ok, #state{}}.
 
-handle_call({start_rt_gen, Name, Price, Volatility, Frequency}, _From, State) ->
-    Reply = spawn_link(estockd_generator, rt_generator, [ #rt_state{
-					    name = Name,
-					    price = Price,
-					    volatility = Volatility,
-					    frequency = Frequency
-					   } ]),
+handle_call({start_rt_gen, Name, Price, Volatility, Frequency, MinPrice, MaxPrice}, _From, State) ->
+    Reply = spawn_link(estockd_generator, rt_generator, 
+					   [ #rt_state{
+							name = Name,
+							price = Price,
+							volatility = Volatility,
+							frequency = Frequency,
+							min_price = MinPrice,
+							max_price = MaxPrice
+						   } 
+					   ]),
     {reply, Reply, State}.
 
 handle_cast(_Msg, State) ->
@@ -71,21 +78,24 @@ code_change(_OldVsn, State, _Extra) ->
 
 rt_generator(State) ->
     receive
-	stop ->
-	    io:format("Done ~p~n", [State#rt_state.name]);
-	_ ->
-	    io:format("Unknown message~n", []),
-	    rt_generator(State)
+		stop ->
+			io:format("Done ~p~n", [State#rt_state.name]);
+		_ ->
+			io:format("Unknown message~n", []),
+			rt_generator(State)
     after State#rt_state.frequency ->
-	    Price = gen_rnd:random_walk(
-		      State#rt_state.price,
-		      6,
-		      State#rt_state.volatility),
-	    estockd:add_row(#stock_row {
-			       price = Price,
-			       name = State#rt_state.name,
-			       amount = random:uniform(100),
-			       timestamp = datetime_util:epoch_millis()
-			      }),
-	    rt_generator(State#rt_state {price = Price})
+			Price = gen_rnd:random_walk(
+					  State#rt_state.price,
+					  State#rt_state.volatility,
+					  ?STEPS,
+					  State#rt_state.min_price,
+					  State#rt_state.max_price ),
+			estockd:add_row(
+			  #stock_row {
+				 price = Price,
+				 name = State#rt_state.name,
+				 amount = random:uniform(100),
+				 timestamp = datetime_util:epoch_millis()
+				}),
+			rt_generator(State#rt_state {price = Price})
     end.
--- apps/estockd/src/gen_rnd.erl
+++ apps/estockd/src/gen_rnd.erl
@@ -6,9 +6,13 @@
 %%% Created : 28 Apr 2012 by Andrey Stepachev <octo47@gmail.com>
 
 -module(gen_rnd).
--export([random_walk/2, random_walk/3]).
+-export([random_walk/2, random_walk/3, random_walk/5]).
 
 -include_lib("eunit/include/eunit.hrl").
+
+-define(DEFAULT_STEPS, 6).
+-define(MIN_PRICE, 3.0).
+-define(MAX_PRICE, 1000.0).
 
 %% ===================================================================
 %% Public API
@@ -16,10 +20,18 @@
 
 -spec random_walk(float(), float()) -> float().
 random_walk(Price, Volatility) ->
-    Price + random_walk_i(6, Volatility).
--spec random_walk(float(), integer(), float()) -> float().
-random_walk(Price, Steps, Volatility) ->
-    Price + random_walk_i(Steps, Volatility).
+    random_walk(Price, Volatility, ?DEFAULT_STEPS).
+-spec random_walk(float(), float(), integer(), float(), float()) -> float().
+random_walk(Price, Volatility, Steps) ->
+	random_walk(Price, Volatility, Steps, ?MIN_PRICE, ?MAX_PRICE).
+-spec random_walk(float(), float(), integer()) -> float().
+random_walk(Price, Volatility, Steps, MinPrice, MaxPrice) ->
+    NewPrice = Price + random_walk_i(Steps, Volatility),
+	if NewPrice < MinPrice -> MinPrice;
+	   NewPrice > MaxPrice -> MaxPrice;
+	   true -> NewPrice
+	end.
+
 
 %% ===================================================================
 %% Internal functions
@@ -27,7 +39,7 @@ random_walk(Price, Steps, Volatility) ->
 
 random_walk_i(Step, _) when Step =:= 0 -> 0;
 random_walk_i(Step, Volatility)->
-    (random:uniform() - 0.485) * 2 * Volatility + random_walk_i(Step-1, Volatility).
+    (random:uniform() - 0.5) * 2 * Volatility + random_walk_i(Step-1, Volatility).
 
 %% ===================================================================
 %% Unit Tests
@@ -35,25 +47,25 @@ random_walk_i(Step, Volatility)->
 
 -ifdef(EUNIT).
 
-price_list(_, N, _) when N =:= 0 -> [];
-price_list(Price, N, Volatility) ->
-    NewPrice = random_walk(Price, Volatility),
-    [NewPrice] ++ price_list(NewPrice, N-1, Volatility).
+price_list(_, N, _, _, _) when N =:= 0 -> [];
+price_list(Price, N, Volatility, MinPrice, MaxPrice) ->
+    NewPrice = random_walk(Price, Volatility, 6, MinPrice * 1.0, MaxPrice * 1.0),
+    [NewPrice] ++ price_list(NewPrice, N-1, Volatility, MinPrice, MaxPrice).
 
-price_list_indexed(Price, N, Volatility) ->
-    lists:zip(lists:seq(1, N), price_list(Price, N, Volatility)).
+price_list_indexed(Price, N, Volatility, MinPrice, MaxPrice) ->
+    lists:zip(lists:seq(1, N), price_list(Price, N, Volatility, MinPrice, MaxPrice)).
 
-dump_to_file(FileName, Amount, Price, Volatility) ->
+dump_to_file(FileName, Amount, Price, Volatility, MinPrice, MaxPrice) ->
     {ok, F}=file:open(FileName, [read,write]),
     file:write(F, [ integer_to_list(Id) ++ " " 
 		    ++ float_to_list(Val) ++ "\n"
-		    || { Id, Val } <-price_list_indexed(Price, Amount, Volatility) ]),
+		    || { Id, Val } <-price_list_indexed(Price, Amount, Volatility, MinPrice, MaxPrice) ]),
     file:close(F).
 
 rnd_test() ->
-    dump_to_file("10_vol.txt", 1500, 60, 1),
-    dump_to_file("07_vol.txt", 1500, 60, 0.7),
-    dump_to_file("03_vol.txt", 1500, 60, 0.3),
-    dump_to_file("01_vol.txt", 1500, 60, 0.1).
+    dump_to_file("10_vol.txt", 3500, 60, 1, 20, 120),
+    dump_to_file("07_vol.txt", 3500, 60, 0.7, 50, 120),
+    dump_to_file("03_vol.txt", 3500, 60, 0.3, 20, 120),
+    dump_to_file("01_vol.txt", 3500, 60, 0.1, 50, 70).
 
 -endif.
