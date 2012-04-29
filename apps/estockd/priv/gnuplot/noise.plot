1ab9b8f6e0bde9743eef383be84ddc271c6a50ff
Andrey Stepachev <octo@yandex-team.ru>
2012-04-29 16:01:03 +0400

    improved random walk

--- apps/estockd/priv/gnuplot/noise.plot
+++ apps/estockd/priv/gnuplot/noise.plot
@@ -4,6 +4,12 @@ set out "/var/tmp/bff.png"
 set encoding koi8r
 set xlabel "X" font "Helvetica,18"
 set ylabel "Noise Amplitude" font "Helvetica,18"
-set style line 1 lt 1 pt 0
-plot "./apps/estockd/.eunit/bff.txt" using 2 title "Generated Noise" with linespoints linestyle 1
+set style line 1 lt 1 lw 1 pt 0
+set style line 2 lt 2 lw 1 pt 0
+set style line 3 lt 3 lw 1 pt 0
+set style line 4 lt 4 lw 1 pt 0
+plot 	"./apps/estockd/.eunit/10_vol.txt" using 2 with linespoints linestyle 1, \
+	"./apps/estockd/.eunit/07_vol.txt" using 2 with linespoints linestyle 2, \
+	"./apps/estockd/.eunit/03_vol.txt" using 2 with linespoints linestyle 3, \
+	"./apps/estockd/.eunit/01_vol.txt" using 2 with linespoints linestyle 4
 
--- apps/estockd/src/gen_rnd.erl
+++ apps/estockd/src/gen_rnd.erl
@@ -10,11 +10,11 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-random_walk(Price, Steps) ->
-    Price + random_walk(Steps).
-random_walk(Step) when Step =:= 0 -> 0;
-random_walk(Step)->
-    (random:uniform() - 0.485) * 2 + random_walk(Step-1).
+random_walk(Price, Steps, Volatility) ->
+    Price + random_walk(Steps, Volatility).
+random_walk(Step, _) when Step =:= 0 -> 0;
+random_walk(Step, Volatility)->
+    (random:uniform() - 0.485) * 2 * Volatility + random_walk(Step-1, Volatility).
 
 noise(T, Seed) ->
     noise_sin_i(T, 1, Seed).
@@ -38,22 +38,25 @@ noise_list(N, Harmonics) ->
 
 -ifdef(EUNIT).
 
-price_list(_, N) when N =:= 0 -> [];
-price_list(Price, N) ->
-    NewPrice = random_walk(Price, 2),
-    [NewPrice] ++ price_list(NewPrice, N-1).
+price_list(_, N, _) when N =:= 0 -> [];
+price_list(Price, N, Volatility) ->
+    NewPrice = random_walk(Price, 6, Volatility),
+    [NewPrice] ++ price_list(NewPrice, N-1, Volatility).
 
-price_list_indexed(Price, N) ->
-    lists:zip(lists:seq(1, N), price_list(Price, N)).
+price_list_indexed(Price, N, Volatility) ->
+    lists:zip(lists:seq(1, N), price_list(Price, N, Volatility)).
 
-dump_to_file(FileName, Amount, Price) ->
+dump_to_file(FileName, Amount, Price, Volatility) ->
     {ok, F}=file:open(FileName, [read,write]),
     file:write(F, [ integer_to_list(Id) ++ " " 
 		    ++ float_to_list(Val) ++ "\n"
-		    || { Id, Val } <-price_list_indexed(Price, Amount) ]),
+		    || { Id, Val } <-price_list_indexed(Price, Amount, Volatility) ]),
     file:close(F).
 
 rnd_test() ->
-    dump_to_file("bff.txt", 2000, 60).
+    dump_to_file("10_vol.txt", 1500, 60, 1),
+    dump_to_file("07_vol.txt", 1500, 60, 0.7),
+    dump_to_file("03_vol.txt", 1500, 60, 0.3),
+    dump_to_file("01_vol.txt", 1500, 60, 0.1).
 
 -endif.
