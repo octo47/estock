2e2ac4d6553a2d94d5b96a745822083a56509f78
Andrey Stepachev <octo@yandex-team.ru>
2012-05-05 19:32:07 +0400

    lazy seq for lists

--- apps/estockd/src/lazy.erl
+++ apps/estockd/src/lazy.erl
@@ -1,10 +1,19 @@
 -module(lazy).
 -compile(export_all).
 
+-include_lib("eunit/include/eunit.hrl").
+
+
 seq(M, N) when M =< N ->
     fun() -> [ M, seq(M+1, N) ] end;
 seq(_, _) ->
     fun() -> [] end.
+
+seq([]) ->
+	fun() -> [] end;
+seq(List) ->
+	[ H | T ] = List,
+    fun() -> [ H, seq(T) ] end.
 
 map_seq(Seq, P) ->
     case Seq() of
@@ -18,5 +27,3 @@ map_seq(Seq, P) ->
 map_seq(M, N, P) ->
     Seq = seq(M, N),
     map_seq(Seq, P).
-
-	    
